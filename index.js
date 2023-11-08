9990433d9bff6f71754b58982d0ee68b1fc8a47f
yowsanx <aboehasanalanshori@gmail.com>
2023-11-08 18:13:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,37 +2,42 @@ const tokenString = document.getElementById("token");
 const errorMessage = document.getElementById("error");
 const message = document.getElementById("message");
 
-// Minta izin notifikasi
+
 if ('Notification' in window) {
+  if (localStorage.getItem('notificationPermission') === 'granted') {
+   showNotification('Notifikasi', 'Notifikasi dari PWA Abu Hasan Al Anshori telah diaktifkan.');
+  } else {
+    requestNotificationPermission();
+  }
+}
+
+function requestNotificationPermission() {
   Notification.requestPermission()
     .then(function (permission) {
       if (permission === 'granted') {
         var userResponse = window.confirm('Izinkan Notifikasi?');
         if (userResponse) {
-        alert('Makasih kakak... :v ');
+          alert('Makasih kakak... :v ');
+          showNotification('Notifikasi', 'Notifikasi dari PWA Abu Hasan Al Anshori telah diaktifkan.');
+          localStorage.setItem('notificationPermission', 'granted');
         } else {
         alert('Yah ditolak... :( ');
+        localStorage.setItem('notificationPermission', 'denied');
         }
       } else if (permission === 'denied') {
-        var userResponse = window.confirm('Izinkan Notifikasi?');
-        if (userResponse) {
-        alert('Makasih kakak... :v ');
-        } else {
-        alert('Yah ditolak... :( ');
-        }
+        localStorage.setItem('notificationPermission', 'denied');
+        alert('Izin notifikasi telah ditolak');
       }
     });
 }
 
-  
-  // Fungsi untuk menampilkan notifikasi
-  function showNotification(title, message) {
-    if ('Notification' in window && Notification.permission === 'granted') {
-      navigator.serviceWorker.ready.then(function (registration) {
-            registration.showNotification(title, {
-            body: message,
-            icon: './assets/images/hello-icon-144.png'
-            });
-        });
-    }
+function showNotification(title, message) {
+  if ('Notification' in window && localStorage.getItem('notificationPermission') === 'granted') {
+    navigator.serviceWorker.ready.then(function (registration) {
+      registration.showNotification(title, {
+        body: message,
+        icon: './assets/images/hello-icon-144.png'
+      });
+    });
+  }
 }
